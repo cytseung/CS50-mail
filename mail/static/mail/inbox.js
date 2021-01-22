ce7217bf92bfd980c844fe2504f6ff6d72c37e20
cyt <NA>
2021-01-22 17:20:44 +0800

    send mail function, works in chrome but not in firefox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,17 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // send email
+  document.querySelector('#send').addEventListener('click', ()=>{
+    let recipients = document.querySelector('#compose-recipients').value;
+    let subject = document.querySelector('#compose-subject').value;
+    let body = document.querySelector('#compose-body').value;
+    send_email(recipients, subject, body);
+  });
+  
+
+  
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +41,23 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(recipients, subject, body){
+  // console.log(`${recipients},${subject},${body}`);
+  fetch('/emails',{
+    method: 'POST',
+    body: JSON.stringify({
+    recipients: recipients,
+    subject: subject,
+    body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
